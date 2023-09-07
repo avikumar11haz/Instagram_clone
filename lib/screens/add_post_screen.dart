60b1ec8b39dd0f46da62120e87d99ff03847de81
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-09-07 23:02:19 +0530

    remove some data

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -25,11 +25,11 @@ class _AddPostScreenState extends State<AddPostScreen> {
       context: parentContext,
       builder: (BuildContext context) {
         return SimpleDialog(
-          title: const Text('Create a Post'), //create a post
+          title: const Text('Create a Post'),
           children: <Widget>[
             SimpleDialogOption(
                 padding: const EdgeInsets.all(20),
-                child: const Text('Take a photo'), // take a photo
+                child: const Text('Take a photo'),
                 onPressed: () async {
                   Navigator.pop(context);
                   Uint8List file = await pickImage(ImageSource.camera);
@@ -39,7 +39,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
                 }),
             SimpleDialogOption(
                 padding: const EdgeInsets.all(20),
-                child: const Text('Choose from Gallery'), //choose from the gallery
+                child: const Text('Choose from Gallery'),
                 onPressed: () async {
                   Navigator.of(context).pop();
                   Uint8List file = await pickImage(ImageSource.gallery);
@@ -49,7 +49,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
                 }),
             SimpleDialogOption(
               padding: const EdgeInsets.all(20),
-              child: const Text("Cancel"),  //cancel
+              child: const Text("Cancel"),
               onPressed: () {
                 Navigator.pop(context);
               },
